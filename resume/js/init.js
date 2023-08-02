fb703c51b1c6945bd78d1b9eb5341849768f8abe
KAMAH <limitlesskds@gmail.com>
2023-08-02 17:14:04 +0100

    MChanged some texts in html && changed the mail client to postmark for sending mails

--- resume/js/init.js
+++ resume/js/init.js
@@ -157,7 +157,7 @@ contactForm.addEventListener('submit', (e) => {
    };
 
    let xhr = new XMLHttpRequest();
-   xhr.open('POST', 'https://kamah.herokuapp.com/submit', true);
+   xhr.open('POST', 'kamah.onrender.com/submit', true);
    xhr.setRequestHeader('content-type', 'application/json');
    xhr.onload = function(){
       if(xhr.responseText == 'success'){
--- resume/server.js
+++ resume/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {google} = require('googleapis')
 const cors = require('cors');
 
 require('dotenv').config({debug:true});
-const nodemailer = require('nodemailer')
+
+const postmark = require('postmark');
+const mail = new postmark.ServerClient(process.env.MAIL_ID);
 
 
 const app = express();
@@ -13,59 +14,24 @@ app.use(express.json())
 app.use(cors())
 
 
-//OAuth2 Configurations
-const oauth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, 
-    process.env.CLIENT_SECRET, process.env.REDIRECT_URL);
-
-oauth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN
-});
-
-const access_token = oauth2Client.getAccessToken()
-
-
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/index.html')
 });
 
 
 app.post("/submit", (req, res) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: process.env.EMAIL_ID,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN,
-            accessToken: access_token
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
+    mail.sendEmail({
+        From: 'cc@uppist.com',
+        To: process.env.EMAIL,
+        Subject: req.body.subject,
+        HtmlBody: `<h1> mail: ${req.body.email} <br> ${req.body.text} </h1>`,
+        MessageStream: 'outbound'
     });
-
-    const mailOptions = {
-        from: `${req.body.names} <${req.body.email}>`,
-        to: process.env.EMAIL_ID,
-        subject: req.body.subject,
-        text: `<${req.body.email}>: \n \n ${req.body.message}`
-    }
-
-    transporter.sendMail(mailOptions, (error, info) =>{
-        if(error){
-            console.log(error);
-            res.send(error);
-        }else{
-            console.log('Email sent ' + info.response);
-            res.send('success')
-            
-        }
-    })
+    res.status(200).send("success")
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`app listening on port ${PORT}`);
